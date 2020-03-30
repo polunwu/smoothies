47b44bfcb2458bf9d338d3f3c84bca2b26c1e63c
Polun <polunwu@gmail.com>
2020-03-30 09:32:17 +0800

    add Navbar Component

---
+++ src/components/Navbar.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="navbar">
+    <nav class="nav-extended indigo darken-2">
+      <div class="nav-content">
+        <router-link to exact>
+          <span class="nav-title">Smoothies</span>
+        </router-link>
+        <a class="btn-floating btn-large halfway-fab pink">
+          <router-link to exact>
+            <i class="material-icons">add</i>
+          </router-link>
+        </a>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+  data() {
+    return {};
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.navbar nav {
+  padding: 0 20px;
+}
+</style>

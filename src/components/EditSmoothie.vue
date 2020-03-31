c398f50b968ced81439aaa9fdef2bc8d1c5672e0
Polun <polunwu@gmail.com>
2020-03-31 23:55:19 +0800

    add EditSmoothie components and router-link

---
+++ src/components/EditSmoothie.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="edit-smoothie container">
+    <h2>Edit a smoothie {{ this.$route.params.smoothie_slug}}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "EditSmoothie",
+  created() {},
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style>
+</style>
